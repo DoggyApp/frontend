c04e568a20e24d081c7dfdf936428195517332da
Ian Martin <idanzigm@gmail.com>
2026-05-15 16:40:41 -0400

    resolved failing test by adding modlues to .spec for new component org registration

--- src/app/organization-components/org-register/org-register.component.spec.ts
+++ src/app/organization-components/org-register/org-register.component.spec.ts
@@ -1,4 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { FormsModule } from '@angular/forms';
+
 import { OrgRegisterComponent } from './org-register.component';
 
 describe('OrgRegisterComponent', () => {
@@ -7,9 +11,12 @@ describe('OrgRegisterComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [OrgRegisterComponent]
+      declarations: [OrgRegisterComponent],
+      imports: [RouterTestingModule, HttpClientTestingModule, FormsModule]
     }).compileComponents();
+  });
 
+  beforeEach(() => {
     fixture = TestBed.createComponent(OrgRegisterComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
